b80ecdc9dbdf8792668e30001eac5011fbbbc9da
ousttrue <ousttrue@gmail.com>
2018-12-12 03:11:47 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# emoji
+
+Windows build of below code.
+
+https://gist.github.com/jokertarot/7583938
---
+++ run.bat
@@ -0,0 +1,2 @@
+set PATH = "C:\vcpkg\installed\x64-windows\bin;%PATH%"
+.\build\clfontpng.exe .\NotoColorEmoji.ttf "😇"
